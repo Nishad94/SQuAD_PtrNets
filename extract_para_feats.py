96fd31aef391e7fd79edfbfdd075c7170a98ddce
Nishad94 <nishad.dawkhar94@gmail.com>
2019-12-10 13:59:31 -0500

    Changed extract dimensions for ha

--- extract_para_feats.py
+++ extract_para_feats.py
@@ -37,6 +37,9 @@ for i_batch, sample_batched in enumerate(iterator):
     emb = getQuestionBertEmbeddings(train_batch_quest_text[0])
     emb = emb.squeeze(0)
     emb = emb.unsqueeze(1).unsqueeze(2)
+    emb = torch.cat([emb,emb,emb],dim=0)[:2048,:,:]
+    import pdb
+    pdb.set_trace()
     context_feats.append(emb)
 
 concat_feats = torch.stack(context_feats)
